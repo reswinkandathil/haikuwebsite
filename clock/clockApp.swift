f524f78e76705d072759abe50ad0372960a89d08
reswin <reswink17@Gmail.com>
2026-03-07 12:54:28 -0600

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Reswin Kandathil on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
